8eb9970a5e94eb0c3a8f1028fc5df02b0739c54c
Joris Huinink <joris.huinink@gmail.com>
2019-04-19 21:50:48 +0200

    Begin refactoring

--- main.js
+++ main.js
@@ -1,54 +1,22 @@
-let lijst = 1;
-let aanwezig = 2;
+let aanwezig = 1;
+let betaald = 2;
 let onkosten = 3;
-let balans = 4;
 
 let app = new Vue({
   el: '#app',
   data: {
-    headersDrinkers: [],
     drinkers: [],
-    headersAanwezigen: [],
-    aanwezigen: [],
-    headersSchuldeisers: [],
-    schuldeisers: [],
-    headersBalans: [],
-    balans: [],
+    dates: []
   },
   mounted() {
     let url;
     let self = this;
 
-    //Gezopen lijst
-    url = createUrl(lijst);
-    $.getJSON(url, function(data) {
-      let entry = data.feed.entry;
-      self.headersDrinkers = getHeaders(entry);
-      self.drinkers = getDrinkers(entry, self.headersDrinkers);
-    })
-
-    //Aanwezig lijst
+    //Haal waarden van aanwezig datum op
     url = createUrl(aanwezig);
     $.getJSON(url, function(data) {
       let entry = data.feed.entry;
-      self.headersAanwezigen = getHeaders(entry);
-      self.aanwezigen = getAanwezigen(entry, self.headersAanwezigen);
-    })
-
-    //Schuldeisers
-    url = createUrl(onkosten);
-    $.getJSON(url, function(data) {
-      let entry = data.feed.entry;
-      self.headersSchuldeisers = getHeaders(entry);
-      self.schuldeisers = getEisers(entry, self.headersSchuldeisers);
-    })
-
-    //Balans
-    url = createUrl(balans);
-    $.getJSON(url, function(data) {
-      let entry = data.feed.entry;
-      self.headersBalans = getHeaders(entry);
-      self.balans = getEisers(entry, self.headersBalans);
+      self.drinkers = getDrinkers(entry);
     })
   }
 })
@@ -57,17 +25,6 @@ function createUrl(tab) {
   return 'https://spreadsheets.google.com/feeds/cells/1UivtJswE_PLcLG2c4MdsHEou9Uvq1uL0s0eFTokJs6E/' + tab + '/public/values?alt=json';
 }
 
-function getHeaders(entry) {
-  let headers = [];
-
-  //Get values of headers
-  for (let i = 0; i < entry.length; i++) {
-    if (entry[i].gs$cell.row == 1) {
-      headers.push(entry[i].content.$t);
-    }
-  }
-  return headers;
-}
 
 function getDrinkers(entry, headers) {
   let drinkers = [];
@@ -99,4 +56,8 @@ function getDrinkers(entry, headers) {
     }
   }
   return drinkers;
+}
+
+function countHeaders(entry) {
+
 }
